0616773a566ea723e16c8458df2ba07704507779
MHS <hayaruby7909@gmail.com>
2025-01-17 21:26:21 +0900

    Feat: border 제거

--- src/pages/admin/component/AdminUserList.js
+++ src/pages/admin/component/AdminUserList.js
@@ -1,7 +1,12 @@
+import './AdminUserList.css';
+
 export default function AdminUserList() {
     return (
-        <div>
+        <div className="admin-user-list-container">
             <h1>User List</h1>
+            <table>
+                
+            </table>
         </div>
     );
 }
