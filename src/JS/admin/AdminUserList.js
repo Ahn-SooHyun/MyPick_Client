0150e71584d31b8e69e0fa4193fc2a439575f508
MHS <hayaruby7909@gmail.com>
2025-01-21 09:39:26 +0900

    Feat: chat 추가

--- src/JS/admin/AdminUserList.js
+++ src/JS/admin/AdminUserList.js
@@ -146,38 +146,39 @@ function UserDeatail({ info, handleClose }) {
     const [chatOpen, setChatOpen] = useState(null);    //채팅방 클릭 유무
 
 
-    const [chatList, setChatList] = useState([
+    const [chatList, setChatList] = useState(
+      [
         {
-            id: 1,
-            title: '채팅 방 1',
-            lastChat: '2025-01-18',
+          id: 1,
+          title: '즐거운 방',
+          lastChat: '2025-01-21', // 오늘 날짜
         },
         {
-            id: 2,
-            title: '채팅 방 2',
-            lastChat: '2025-01-17',
+          id: 2,
+          title: '수다 떨기 좋은 방',
+          lastChat: '2025-01-20', // 어제 날짜
         },
         {
-            id: 3,
-            title: '채팅 방 3',
-            lastChat: '2025-01-13',
+          id: 3,
+          title: '이야기 나누는 방',
+          lastChat: '2025-01-17',
         },
         {
-            id: 4,
-            title: '채팅 방 4',
-            lastChat: '2025-01-04',
+          id: 4,
+          title: '비밀스런 방',
+          lastChat: '2025-01-10',
         },
         {
-            id: 5,
-            title: '채팅 방 5',
-            lastChat: '2025-01-05',
+          id: 5,
+          title: '웃음 가득 방',
+          lastChat: '2025-01-05',
         },
         {
-            id: 6,
-            title: '채팅 방 6',
-            lastChat: '2025-01-06',
+          id: 6,
+          title: '새로운 만남 방',
+          lastChat: '2025-01-02',
         },
-    ]);
+      ]);
     
     // info가 변경될 때만 step도 업데이트되도록 useEffect 내에서 처리
     useEffect(() => {
@@ -389,7 +390,7 @@ function UserDeatail({ info, handleClose }) {
             </div>
 
             
-            {chatOpen && <ChatContainer eventClose={() => setChatOpen(null)}/>}
+            {chatOpen && <ChatContainer eventClose={() => setChatOpen(null)} info={chatOpen}/> }
     
         </div>
     );
@@ -397,11 +398,76 @@ function UserDeatail({ info, handleClose }) {
 
 function ChatContainer({ info, eventClose }) {
 
+
+
     /***info 안에는 chatList idx, chatList가 열은 userId가 들어 있음.  */
-    const [target, setTarget] = useState({'userId': 'a1', 'chatIdx': 1});
+    const [target, setTarget] = useState({'userId': 'a1', 'chatIdx': info});
+
+    const chatList = [
+        {
+          userId: 'a2',
+          content: '네, 준비물은 각자 챙기고, 물과 간단한 간식 정도만 준비하면 될 것 같아요. 그럼 주말에 뵙겠습니다!',
+          hour: 16,
+          minute: 30,
+        },
+        {
+          userId: 'a1',
+          content: '좋습니다! 그럼 토요일 아침 7시에 만나요. 준비물은 각자 챙기고, 물과 간단한 간식 정도만 준비하면 될 것 같아요. 그럼 주말에 뵙겠습니다!',
+          hour: 16,
+          minute: 20,
+        },
+        {
+          userId: 'a2',
+          content: '네, 아침 일찍 출발하는 게 좋을 것 같아요. 그럼 토요일 아침 7시에 출발하는 걸로 할까요? 그 시간에 출발하면 사람도 많지 않고, 여유롭게 등산할 수 있을 것 같아요.',
+          hour: 15,
+          minute: 50,
+        },
+        {
+          userId: 'a1',
+          content: '좋아요! 그럼 이번 주말에 북한산으로 가는 걸로 할까요? 시간은 어떻게 되세요? 아침 일찍 출발하는 게 좋을 것 같아요.',
+          hour: 15,
+          minute: 20,
+        },
+        {
+          userId: 'a2',
+          content: '저는 북한산을 생각하고 있었어요. 북한산은 경치도 좋고, 등산로도 잘 되어 있어서 초보자도 쉽게 갈 수 있거든요. 그리고 근처에 맛집도 많아서 등산 후에 맛있는 음식도 먹을 수 있을 것 같아요.',
+          hour: 14,
+          minute: 55,
+        },
+        {
+          userId: 'a1',
+          content: '그리고 등산 후에는 근처 맛집에서 맛있는 음식도 먹으면 좋을 것 같아요. 등산 후에 먹는 음식은 정말 꿀맛이잖아요!',
+          hour: 14,
+          minute: 30,
+        },
+        {
+          userId: 'a1',
+          content: '등산 좋죠! 저도 등산을 좋아해서 자주 가는데, 이번 주말에 시간이 되면 같이 가고 싶어요. 어느 산을 가실 계획인가요?',
+          hour: 13,
+          minute: 50,
+        },
+        {
+          userId: 'a2',
+          content: '혹시 주말에 같이 등산 가실 분 계신가요? 요즘 날씨가 좋아서 등산하기에 딱 좋은 것 같아요. 산 정상에서 보는 경치가 정말 멋질 것 같아요.',
+          hour: 13,
+          minute: 10,
+        },
+        {
+          userId: 'a2',
+          content: '맞아요, 저도 오늘 아침에 일어나서 창문을 열어보니 햇살이 너무 따뜻하고 기분이 좋더라고요. 그래서 아침 운동을 좀 했어요. 그리고 나서 커피 한 잔 마시면서 책을 읽고 있었어요.',
+          hour: 12,
+          minute: 45,
+        },
+        {
+          userId: 'a1',
+          content: '안녕하세요! 오늘 날씨가 정말 좋네요. 이런 날씨에는 산책을 가거나 야외 활동을 하기에 딱 좋은 것 같아요. 여러분은 오늘 어떤 계획이 있으신가요?',
+          hour: 11,
+          minute: 20,
+        },
+      ];
 
     //** 채팅 내용 리스트 */
-    const chatContentList = [
+    const chatContentList = target.chatIdx %2 === 0 ?[
         {
             userId: 'a1',
             content: '안녕하세요! 오늘 날씨가 정말 좋네요. 이런 날씨에는 산책을 가거나 야외 활동을 하기에 딱 좋은 것 같아요. 여러분은 오늘 어떤 계획이 있으신가요?',
@@ -442,7 +508,69 @@ function ChatContainer({ info, eventClose }) {
             userId: 'a2',
             content: '네, 준비물은 각자 챙기고, 물과 간단한 간식 정도만 준비하면 될 것 같아요. 그럼 주말에 뵙겠습니다!',
         },
+    ] : [
+      {
+        userId: 'a2',
+        content: '네, 준비물은 각자 챙기고, 물과 간단한 간식 정도만 준비하면 될 것 같아요. 그럼 주말에 뵙겠습니다!',
+        hour: 16,
+        minute: 30,
+      },
+      {
+        userId: 'a1',
+        content: '좋습니다! 그럼 토요일 아침 7시에 만나요. 준비물은 각자 챙기고, 물과 간단한 간식 정도만 준비하면 될 것 같아요. 그럼 주말에 뵙겠습니다!',
+        hour: 16,
+        minute: 20,
+      },
+      {
+        userId: 'a2',
+        content: '네, 아침 일찍 출발하는 게 좋을 것 같아요. 그럼 토요일 아침 7시에 출발하는 걸로 할까요? 그 시간에 출발하면 사람도 많지 않고, 여유롭게 등산할 수 있을 것 같아요.',
+        hour: 15,
+        minute: 50,
+      },
+      {
+        userId: 'a1',
+        content: '좋아요! 그럼 이번 주말에 북한산으로 가는 걸로 할까요? 시간은 어떻게 되세요? 아침 일찍 출발하는 게 좋을 것 같아요.',
+        hour: 15,
+        minute: 20,
+      },
+      {
+        userId: 'a2',
+        content: '저는 북한산을 생각하고 있었어요. 북한산은 경치도 좋고, 등산로도 잘 되어 있어서 초보자도 쉽게 갈 수 있거든요. 그리고 근처에 맛집도 많아서 등산 후에 맛있는 음식도 먹을 수 있을 것 같아요.',
+        hour: 14,
+        minute: 55,
+      },
+      {
+        userId: 'a1',
+        content: '그리고 등산 후에는 근처 맛집에서 맛있는 음식도 먹으면 좋을 것 같아요. 등산 후에 먹는 음식은 정말 꿀맛이잖아요!',
+        hour: 14,
+        minute: 30,
+      },
+      {
+        userId: 'a1',
+        content: '등산 좋죠! 저도 등산을 좋아해서 자주 가는데, 이번 주말에 시간이 되면 같이 가고 싶어요. 어느 산을 가실 계획인가요?',
+        hour: 13,
+        minute: 50,
+      },
+      {
+        userId: 'a2',
+        content: '혹시 주말에 같이 등산 가실 분 계신가요? 요즘 날씨가 좋아서 등산하기에 딱 좋은 것 같아요. 산 정상에서 보는 경치가 정말 멋질 것 같아요.',
+        hour: 13,
+        minute: 10,
+      },
+      {
+        userId: 'a2',
+        content: '맞아요, 저도 오늘 아침에 일어나서 창문을 열어보니 햇살이 너무 따뜻하고 기분이 좋더라고요. 그래서 아침 운동을 좀 했어요. 그리고 나서 커피 한 잔 마시면서 책을 읽고 있었어요.',
+        hour: 12,
+        minute: 45,
+      },
+      {
+        userId: 'a1',
+        content: '안녕하세요! 오늘 날씨가 정말 좋네요. 이런 날씨에는 산책을 가거나 야외 활동을 하기에 딱 좋은 것 같아요. 여러분은 오늘 어떤 계획이 있으신가요?',
+        hour: 11,
+        minute: 20,
+      },
     ];
+
 
     const chatEndRef = useRef(null);
 
@@ -462,7 +590,7 @@ function ChatContainer({ info, eventClose }) {
                     <div className={`chat-bubble ${item.userId === target.userId ? 'my-bubble' : 'other-bubble'}`}>
                         <div className="chat-content-item-content">{item.content}</div>
                         <div className="chat-content-item-time">
-                            {new Date().toLocaleTimeString('ko-KR', {
+                          {new Date().toLocaleTimeString('ko-KR', {
                                 hour: '2-digit',
                                 minute: '2-digit',
                                 hour12: true
